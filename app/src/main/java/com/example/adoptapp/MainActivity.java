6c07b222469b9c0ceb4e19665b8838de664443fb
Cristina <cristina.pope18@gmail.com>
2021-11-09 23:13:28 +0200

    Added signup activity xml and logic

--- app/src/main/java/com/example/adoptapp/MainActivity.java
+++ app/src/main/java/com/example/adoptapp/MainActivity.java
@@ -2,18 +2,32 @@ package com.example.adoptapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
+//import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+        signupButton = (Button) findViewById(R.id.registerButton);
+        signupButton.setOnClickListener(v -> openActivitySignup());
+    }
+    public void openActivitySignup()
+    {
+        Intent signupIntent = new Intent(this, SignUp.class);
+        startActivity(signupIntent);
     }
 }
