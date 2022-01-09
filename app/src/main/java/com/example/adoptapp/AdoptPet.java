1544fd82dadcd28107b28eead5685ca1d670baa2
Cristina <cristina.pope18@gmail.com>
2022-01-10 00:48:42 +0200

    Take pet back screen and small modifications for adopt logic

--- app/src/main/java/com/example/adoptapp/AdoptPet.java
+++ app/src/main/java/com/example/adoptapp/AdoptPet.java
@@ -1,17 +1,28 @@
 package com.example.adoptapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdoptPet extends AppCompatActivity {
 
+    private Button adoptButton;
+    private ImageView petPicture;
+    private TextView petName;
+    private TextView petType;
+    private TextView petDescription;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adopt_pet);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        adoptButton = (Button) findViewById(R.id.adoptButton);
 
+        //adoptButton.setOnClickListener(v -> openActivityAddPet());
     }
 }
---
+++ app/src/main/java/com/example/adoptapp/TakePetBack.java
@@ -0,0 +1,15 @@
+package com.example.adoptapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TakePetBack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
