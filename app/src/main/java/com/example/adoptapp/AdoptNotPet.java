c58b5ee7cede6f06ff35268f21288d4441399c27
Cristina <cristina.pope18@gmail.com>
2022-01-10 01:12:45 +0200

    Added adoptn't pet

--- app/src/main/java/com/example/adoptapp/AdoptNotPet.java
+++ app/src/main/java/com/example/adoptapp/AdoptNotPet.java
@@ -5,11 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TakePetBack extends AppCompatActivity {
-
+public class AdoptNotPet extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_take_pet_back);
+        setContentView(R.layout.activity_adopt_not_pet);
     }
 }
